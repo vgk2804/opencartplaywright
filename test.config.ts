5614a5d5fdb1e610049e802a849bb927ea31fe1b
G K Vinodh <vinodh.gnani6@gmail.com>
2026-03-13 22:26:46 +0530

    first commit

---
+++ test.config.ts
@@ -0,0 +1,20 @@
+export class TestConfig{
+ 
+    //appUrl="http://localhost/opencart/upload/"
+    //appUrl="https://naveenautomationlabs.com/opencart"
+    appUrl="https://tutorialsninja.com/demo/"
+    
+  
+    //valid login credentials
+    email="vinodh.gnani6@gmail.com"
+    password="P@ssw0rd123"
+
+  /*    email="pavanol@abc.com"
+    password="test@123"
+ */
+
+    //product details
+    productName="MacBook"
+    productQuantity="2"
+    totalPrice="$1,204.00"
+}
